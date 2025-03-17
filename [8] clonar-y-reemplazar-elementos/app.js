49ec070c249348cbde961d1927ce380a241256be
Anthony Fuentes <anthony.fuentes2005@gmail.com>
2025-03-17 16:19:50 -0400

    Clase 14

--- [8] clonar-y-reemplazar-elementos/app.js
+++ [8] clonar-y-reemplazar-elementos/app.js
@@ -1,2 +1,25 @@
+// .cloneNode(true) para clonar y .replaceWith(elementoQueVaAReemplazar)
 // Seleccionamos el elemento con el id "contentArea"
-const contentArea = document.querySelector('#contentArea')
+const contentArea = document.querySelector('#contentArea');
+
+// Seleccionamos el <p> del #contentArea
+const pContentArea = contentArea.querySelector('p');
+
+// Clonamos el elemento <p> con .cloneNode(true)
+const cloneP = pContentArea.cloneNode(true);
+
+// Agregamos el elemento clonado al #contentArea
+contentArea.append(cloneP);
+
+// Cambiamos el contenido de texto de cloneP
+cloneP.textContent = 'Este es un nuevo texto'
+
+//Ahora con el replaceWith()
+//Seleccionamos la lista "listArea"
+const lista = document.querySelector('#listArea')
+
+//Seleccionamos el item a reemplazar, de la lista tomaremos el 3 item.
+const itemAReemplazar = listArea.children[2];
+
+// Reemplazamos el itemAReemplazar con cloneP
+itemAReemplazar.replaceWith(cloneP);
